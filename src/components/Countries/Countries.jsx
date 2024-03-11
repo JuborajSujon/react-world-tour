25aece315e1a232eb29b2faa6c7982e84ed79196
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-11 17:42:35 +0600

    conditional css and conditional style

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -5,6 +5,7 @@ import "./Countries.css";
 
 export default function Countries() {
   const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
@@ -13,12 +14,24 @@ export default function Countries() {
         setCountries(data);
       });
   }, []);
+
+  const handleVisitedCountry = (country) => {
+    console.log("Add this to your visited country");
+    console.log(country);
+  };
   return (
     <div>
       <h3>Countries : {countries.length}</h3>
+      <div>
+        <h5>Visited Countries</h5>
+        <ul></ul>
+      </div>
       <div className="country-container">
         {countries.map((country) => (
-          <Country country={country} key={country.cca3}></Country>
+          <Country
+            country={country}
+            key={country.cca3}
+            handleVisitedCountry={handleVisitedCountry}></Country>
         ))}
       </div>
     </div>
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import "./Country.css";
-export default function Country({ country }) {
+export default function Country({ country, handleVisitedCountry }) {
   const { name, flags, population, area, cca3 } = country;
   const [visited, setVisited] = useState(false);
   const handleVisited = () => {
     setVisited(!visited);
   };
+  console.log(handleVisitedCountry);
   return (
-    <div className="country">
-      <h3>{name?.common}</h3>
+    <div className={`country ${visited ? "visited" : "non-visited"}`}>
+      <h3 style={{ color: visited ? "green" : "red" }}>{name?.common}</h3>
       <img src={flags?.png} alt="" />
       <p>Population: {population}</p>
       <p>Area: {area}</p>
@@ -17,6 +18,8 @@ export default function Country({ country }) {
       </p>
       {visited && <p>I have visited this country</p>}
       <button onClick={handleVisited}>Visited</button>
+      <br />
+      <button>Mark visited</button>
     </div>
   );
 }
